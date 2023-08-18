bca48406d00bd3dcdc997a699f99a0b63768ad9f
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 15:46:00 -0400

    ...attempt 3

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -1,29 +1,22 @@
 import { useRouter } from 'next/router'
-import { Html, Head, Main, NextScript } from 'next/document'
-class MyDoc extends Document {
-  router = useRouter();
-  pos = "srcScript";
-  options = {startOnLoad: true, lang: 'en', dontEmbedHeadings: false};
-  render(){
-    return (
-    <Html>
-      <Head>
-        <Main/>
-        <NextScript/>
-        <title>Terminus Industries | Post: {router.query.slug}</title>    
-        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async id={pos}></script>
-        <script>
-            Nutshell.setOptions({options});
-        </script>
-      </Head>
+ 
+export default function Page() {
+  const router = useRouter()
+  var options ={startOnLoad: true, lang: 'en', dontEmbedHeadings:false}
+  return( 
+    <html>
+      <head>
+        <title>Terminus Industries | Post: {router.query.slug}</title>
+        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></script>
+        <script>Nutshell.setOptions({options})</script>
+      </head>
       <body>
-        <h2 hidden="true"></h2>
-        <p>This is a test deployment for dynamic pages. <a href="#FindOutMore">:Find out more...</a></p>
-        <h2>:x Find Out More...</h2>
-        <p>Your url slug parameter setting is as follows: {router.query.slug}</p>
+        <h2>Post: {router.query.slug}</h2>
+        <p>This is a rough test of the system. <a href="#More">:More...</a></p>
+        <h2>:x More</h2>
+        <p>Your input slug for this url was "{router.query.slug}"</p>
       </body>
-    </Html>
-    )
-  }
+      
+    </html>
+  )
 }
-export default MyDoc
