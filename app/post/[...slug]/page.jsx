4d36f4eab5fd0322913641ce2f6ea1ceaff796fe
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 15:06:15 -0400

    proper slug pulling, and removed unnecessary files

---
+++ app/post/[...slug]/page.jsx
@@ -0,0 +1,20 @@
+export default function Page({ params }) {
+  var options = {startOnLoad: true, lang: 'en', dontEmbedHeadings: false};
+  return 
+    <html>
+      <head>
+      <title>Terminus Industries</title>
+        
+        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></script>
+        <script>
+            Nutshell.setOptions({options});
+        </script>
+      </head>
+      <body>
+        <h2 hidden="true"></h2>
+        <p>This is a test deployment for dynamic pages. <a href="#FindOutMore">:Find out more...</a></p>
+        <h2>:x Find Out More...</h2>
+        <p>Your url slug parameter setting is as follows: {params.slug}</p>
+      </body>
+    </html>
+}
