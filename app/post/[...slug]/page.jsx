15f159341f4a62d8d2aaca5d2860aada17e0cc3a
J <58743723+JadedChara@users.noreply.github.com>
2023-08-18 18:36:33 -0400

    Update page.jsx

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -7,19 +7,9 @@ export default function Page() {
   const params = useParams;
   //const slug = params;
   return( 
-    <html>
-      <head>
-        <title>Terminus Industries | Post: {params[0]}</title>
-        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async></script>
-        <script>Nutshell.setOptions(&#123;startOnLoad: true, lang: &#39;en&#39;, dontEmbedHeadings:false&#125;)</script>
-      </head>
-      <body>
         <h2>Post: {params[0]}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More</h2>
         <p>Your input slug for this url was &quot;{params[0]}&quot;</p>
-      </body>
-      
-    </html>
   )
 }
