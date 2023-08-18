b033cc24bd831e3df2d0af8dc77aba02c066253f
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 16:59:29 -0400

    meep.

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -8,15 +8,15 @@ export default function Page(params) {
   return( 
     <html>
       <head>
-        <title>Terminus Industries | Post: {slug}</title>
+        <title>Terminus Industries | Post: {slug.params}</title>
         <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async></script>
         <script>Nutshell.setOptions(&#123;startOnLoad: true, lang: &#39;en&#39;, dontEmbedHeadings:false&#125;)</script>
       </head>
       <body>
-        <h2>Post: {slug}</h2>
+        <h2>Post: {slug.params}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More</h2>
-        <p>Your input slug for this url was &quot;{slug}&quot;</p>
+        <p>Your input slug for this url was &quot;{slug.params}&quot;</p>
       </body>
       
     </html>
