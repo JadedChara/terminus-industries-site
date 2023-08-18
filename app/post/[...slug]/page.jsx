112897ecf69f3a91ffd145d763d3fa1a4fb2254e
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 15:35:07 -0400

    ...and attempt #2

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -1,21 +1,29 @@
-export default function Page({ params }) {
-  var pos = "srcScript";
-  var options = {startOnLoad: true, lang: 'en', dontEmbedHeadings: false};
-  return 
-    <html>
-      <head>
-      <title>Terminus Industries</title>
-        
+import { useRouter } from 'next/router'
+import { Html, Head, Main, NextScript } from 'next/document'
+class MyDoc extends Document {
+  router = useRouter();
+  pos = "srcScript";
+  options = {startOnLoad: true, lang: 'en', dontEmbedHeadings: false};
+  render(){
+    return (
+    <Html>
+      <Head>
+        <Main/>
+        <NextScript/>
+        <title>Terminus Industries | Post: {router.query.slug}</title>    
         <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async id={pos}></script>
         <script>
             Nutshell.setOptions({options});
         </script>
-      </head>
+      </Head>
       <body>
         <h2 hidden="true"></h2>
         <p>This is a test deployment for dynamic pages. <a href="#FindOutMore">:Find out more...</a></p>
         <h2>:x Find Out More...</h2>
-        <p>Your url slug parameter setting is as follows: {params.slug}</p>
+        <p>Your url slug parameter setting is as follows: {router.query.slug}</p>
       </body>
-    </html>
+    </Html>
+    )
+  }
 }
+export default MyDoc
