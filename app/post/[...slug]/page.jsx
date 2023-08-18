4e2b27ae4640b3bdde39acefff7667ca91b52140
J <58743723+JadedChara@users.noreply.github.com>
2023-08-18 19:55:32 -0400

    Update page.jsx

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -8,7 +8,7 @@ export default function Page() {
   //var slug = params;
   return( 
         <h2>{"Post: "}{params[0]}</h2>
-        <p>{"This is a rough test of the system. "}<a href="#More">:More...</a></p>
+        <p>{"This is a rough test of the system."} <a href="#More">:More...</a></p>
         <h2>:x More</h2>
         <p>{"Your input slug for this url was &quot;"}{params[0]}{"&quot;"}</p>
   )
