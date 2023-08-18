3188be79556646c656269f6e1639d40595e82bda
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 17:16:52 -0400

    possible fix, idk

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -1,22 +1,23 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useParams } from 'next/navigation'
  
-export default function Page(params) {
-  const router = useRouter()
-  const slug = params;
+export default function Page() {
+  //const router = useRouter()
+  const params = useParams;
+  //const slug = params;
   return( 
     <html>
       <head>
-        <title>Terminus Industries | Post: {slug.params}</title>
+        <title>Terminus Industries | Post: {params[0]}</title>
         <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async></script>
         <script>Nutshell.setOptions(&#123;startOnLoad: true, lang: &#39;en&#39;, dontEmbedHeadings:false&#125;)</script>
       </head>
       <body>
-        <h2>Post: {slug.params}</h2>
+        <h2>Post: {params[0]}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More</h2>
-        <p>Your input slug for this url was &quot;{slug.params}&quot;</p>
+        <p>Your input slug for this url was &quot;{params[0]}&quot;</p>
       </body>
       
     </html>
