1f09c2c6fc2207d4d71104dfe6669a98ca3462cb
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 16:25:58 -0400

    idfk anymore.

--- app/post/[...slug]/page.jsx
+++ app/post/[...slug]/page.jsx
@@ -1,20 +1,21 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
  
-export default function Page() {
+export default function Page(params) {
   const router = useRouter()
+  const slug = params;
   var options ={startOnLoad: true, lang: 'en', dontEmbedHeadings:false}
   return( 
     <html>
       <head>
-        <title>Terminus Industries | Post: {router.query.slug}</title>
+        <title>Terminus Industries | Post: {slug}</title>
         <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></script>
         <script>Nutshell.setOptions({options})</script>
       </head>
       <body>
-        <h2>Post: {router.query.slug}</h2>
+        <h2>Post: {slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More</h2>
-        <p>Your input slug for this url was "{router.query.slug}"</p>
+        <p>Your input slug for this url was "{slug}"</p>
       </body>
       
     </html>
