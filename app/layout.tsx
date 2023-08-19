a42b1c0118982e6e5b7a01ec35dbf12ccf045e44
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-18 22:19:42 -0400

    AAAAAAA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Head } from 'next/head'
-import { Script } from 'next/script'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,8 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <title>Terminus Industries | Post</title>
-        <Script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async></Script>
-        <Script>Nutshell.setOptions(&#123;startOnLoad: true, lang: &#39;en&#39;, dontEmbedHeadings:false&#125;)</Script>
+        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async></script>
+        <script>Nutshell.setOptions(&#123;startOnLoad: true, lang: &#39;en&#39;, dontEmbedHeadings:false&#125;)</script>
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
