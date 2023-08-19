d5ab5bd470308e65f91d0aa22d28f1a503e1e66d
J <58743723+JadedChara@users.noreply.github.com>
2023-08-19 19:43:10 -0400

    Update page.tsx

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -5,14 +5,12 @@ import RootLayout from "@/app/layout"
  
 export default function Post({ params }: { params: { slug: string } }) {
   //const params = useParams;
-  return( 
-    <RootLayout>
+  return(
       <div>
         <h2>Post: {params.slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More</h2>
         <p>Your input slug for this url was &quot;{params.slug}&quot;</p>
       </div>
-    </RootLayout>
   )
 }
