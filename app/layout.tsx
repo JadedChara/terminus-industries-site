b0d71f40e367f0e1551f452f743b7c8f311bf3fe
J <58743723+JadedChara@users.noreply.github.com>
2023-08-19 19:41:52 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html>
       <Head>
         <title>Terminus Industries | Post</title>
         </Head>
