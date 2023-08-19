73eb0a87d19b8f863ddf0fd205c0f17198195a2f
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-19 16:07:57 -0400

    ****

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -8,10 +8,10 @@ export default function Post({ params }: { params: { slug: string } }) {
   return( 
     <RootLayout>
       <div>
-        <h2>Post: {params.id}</h2>
+        <h2>Post: {params.slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More</h2>
-        <p>Your input slug for this url was &quot;{params.id}&quot;</p>
+        <p>Your input slug for this url was &quot;{params.slug}&quot;</p>
       </div>
     </RootLayout>
   )
