26135fafcf2de150602895ec04eaa5d2445280cf
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-19 16:11:30 -0400

    huh.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
+//import './globals.css'
 import type { Metadata } from 'next'
-import Head from 'next/head'
+//import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,12 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <Head>
+    <html lang="en" background-color="white">
+      <head>
         <title>Terminus Industries | Post</title>
         <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js" async></script>
         <script>Nutshell.setOptions(&#123;startOnLoad: true, lang: &#39;en&#39;, dontEmbedHeadings:false&#125;)</script>
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
