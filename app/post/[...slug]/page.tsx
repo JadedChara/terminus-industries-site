a87856e38890664c2fe45617ec1a77586bfb9d5c
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-20 12:03:05 -0400

    Hyde V3

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -16,11 +16,11 @@ export default function Post({ params }: { params: { slug: string } }) {
   //const params = useParams;
   return(
       <div>
-        <Hyde><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js" defer></script></Hyde>
+        <Hyde><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js" defer></script>
         <h2>Post: {params.slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More </h2>
-        <p>Your input slug for this url was &quot;{params.slug}&quot;</p>
+        <p>Your input slug for this url was &quot;{params.slug}&quot;</p></Hyde>
       </div>
   )
 }
