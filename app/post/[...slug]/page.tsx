0950ff32af11c4e2f43416eba0ba5aa3b5537859
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-20 11:41:08 -0400

    Hyde function V1

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -1,11 +1,18 @@
-'use client'
+//'use client'
 
+function Hyde({children}){
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
 
 export default function Post({ params }: { params: { slug: string } }) {
   //const params = useParams;
   return(
       <div>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js" defer></script>
+        <Hyde><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js" defer></script></Hyde>
         <h2>Post: {params.slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More </h2>
