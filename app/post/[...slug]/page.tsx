c2201f6938cea2cba2054fe86658efac9bca6fe6
J <58743723+JadedChara@users.noreply.github.com>
2023-08-19 20:03:24 -0400

    huh

--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -8,7 +8,7 @@ export default function Post({ params }: { params: { slug: string } }) {
       <div>
         <h2>Post: {params.slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
-        <h2>:x More</h2>
+        <h2>:x More </h2>
         <p>Your input slug for this url was &quot;{params.slug}&quot;</p>
       </div>
   )
