84d616f55ad82ad2e163ed43b074d239bc66cde6
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-19 22:23:41 -0400

    inline nonsensery

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
       <Head>
         <title>Terminus Industries | Post</title>
         </Head>
-        <Script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></Script>
-        <Script>{`Nutshell.setOptions({startOnLoad: true, lang: "en", dontEmbedHeadings:false})`}</Script>
+        <Script id="callNutshell" src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.js"></Script>
+        <Script id="initNutshell">{`Nutshell.setOptions({startOnLoad: true, lang: "en", dontEmbedHeadings:false})`}</Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
