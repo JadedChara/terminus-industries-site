7c62f0cc347dedda0828681ae16c3a80c87a6972
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-08-20 23:11:43 -0400

    ...and now we try, try again.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import Head from 'next/head'
 import Script from 'next/script'
 import { Inter } from 'next/font/google'
-//import { useEffect, useState } from 'react'
+import useSWR from 'swr'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +17,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
+  const {data, error, isLoading} = useSWR(
+    'https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js',fetcher
+  )
+  if (error) return <p>Failed to load.</p>
+  if (isLoading) return <p>Loading...</p>
   return (
     <html lang="en">
       <Head>
         <title>Terminus Industries | Post</title>
+        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js"></script>
+        <h2>Post: {params.slug}</h2>
       </Head>
         
       <body className={inter.className}>{children}</body>
--- app/post/[...slug]/page.tsx
+++ app/post/[...slug]/page.tsx
@@ -2,7 +2,6 @@
 
 //import useEffect from 'react';
 //import useState from 'react';
-import useSWR from 'swr'
 function Hyde({
   children,
 }: {
@@ -17,27 +16,9 @@ function Hyde({
 }
 
 export default function Post({ params }: { params: { slug: string } }) {
-  /*const [loaded, setLoaded] = useState(false);
-  useEffect(()=>{
-    const nutScript = document.createElement('script');
-    nutScript.src = "//cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js";
-    nutScript.addEventListener('load',()=>setLoaded(true));
-
-  },[])
-  useEffect(()=>{
-    if(!loaded){
-      return;
-    }
-  },[loaded])*/
-  const {data, error, isLoading} = useSWR(
-    'https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js',fetcher
-  )
-  if (error) return <p>Failed to load.</p>
-  if (isLoading) return <p>Loading...</p>
+  
   return(
       <div>
-        <script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js"></script>
-        <h2>Post: {params.slug}</h2>
         <p>This is a rough test of the system. <a href="#More">:More...</a></p>
         <h2>:x More </h2>
         <p>Your input slug for this url was &quot;{params.slug}&quot;</p>
