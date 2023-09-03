7799bcf5224e8869a78b684b94a6749e2248f81d
JadedChara <JadedChara.ashadoweddawn@gmail.com>
2023-09-02 22:41:25 -0400

    urgity blurgity rawr

--- app/components/Dynscript.tsx
+++ app/components/Dynscript.tsx
@@ -4,11 +4,12 @@ export default function Dynscript() {
   return (
     <div>
       <Script src="https://cdn.jsdelivr.net/gh/ncase/nutshell/nutshell.min.js" onLoad={()=>{
-        Nutshell.setOptions({
-            startOnLoad: true,
-            lang: 'en', 
-            dontEmbedHeadings: false,
-        })}}/>
+        //Nutshell.setOptions({
+        //    startOnLoad: true,
+        //    lang: 'en', 
+        //    dontEmbedHeadings: false,
+        //})
+    }}/>
     </div>
   )
 }
