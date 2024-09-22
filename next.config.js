7edbe74577c3c71c9a97e5930db39a7eb11c9c8b
58743723+JadedChara@users.noreply.github.com <JadedChara>
2024-09-22 12:24:14 -0400

    Updated to include new project categories...

--- next.config.js
+++ next.config.js
@@ -11,12 +11,28 @@ const nextConfig = {
                 destination:'/pages/about.html'
             },
             {
-                source:'/nightstrike-nook',
+                source:'/nightstrike-s-nook',
                 destination:'/pages/nook.html'
             },
             {
                 source:'/projects',
                 destination:'/pages/projects.html'
+            },
+            {
+                source:'/projects/coding',
+                destination:'/pages/codingprojects.html'
+            },
+            {
+                source:'/projects/papercraft',
+                destination:'/pages/papercraftprojects.html'
+            },
+            {
+                source:'/projects/nightstrike-s-customs',
+                destination:'/pages/tfprojects.html'
+            },
+            {
+                source:'/projects/misc',
+                destination:'/pages/miscprojects.html'
             }
         ]
     }
