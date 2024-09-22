9f87ddbb621c7384498d431a13d93e11d6e93605
JadedChara <ashadoweddawn@gmail.com>
2024-09-22 08:32:24 -0400

    tiniest build error ever.

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
             },
             {
                 source:'/projects',
-                destination:'projects.html'
+                destination:'/projects.html'
             }
         ]
     }
